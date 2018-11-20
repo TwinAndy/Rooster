356cb026e8aa8be67e1560c8b34b5fc7722dd69e
Andrew Lodeizen <20040701@merletcollege.nl>
2018-11-20 10:04:11 +0100

    test

--- index.php
+++ index.php
@@ -12,14 +12,14 @@ class Kalender {
 		$this->jaar = $jaar;
 		$this->dagen_vd_week = $dagen_vd_week;
 		$this->aantal_dagen = cal_days_in_month( CAL_GREGORIAN, $this->maand, $this->jaar );
-		$this->datum_info = getdate( strtotime( mktime( 0,0,0,$this->maand,1,$this->jaar) ) );
+		$this->datum_info = getdate( strtotime('first day of', mktime( 0,0,0,$this->maand,1,$this->jaar) ) );
 		$this->dag_vd_week = $this->datum_info['wday'];
 		$this->dag_vd_week;
 	}
 	public function show(){
 		// Maand en Jaar titel
 		$output = '<table class="kalender">';
-		$output .= '<caption>' . $this->datum_info['month'] . ' ' . $this->jaar . '</caption>';
+		$output .= '<caption>' . $this->datum_info['maand'] . ' ' . $this->jaar . '</caption>';
 		$output .= '<tr>';
 
 		//Dagen van de week header
