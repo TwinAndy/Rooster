9463550937f5803f9516d9cf4ba2ca412b72711b
Andrew Lodeizen <20040701@merletcollege.nl>
2018-11-20 09:43:11 +0100

    test

--- kalender.php
+++ kalender.php
@@ -49,7 +49,6 @@ function build_kalender($maand, $jaar, $datumArray){
 
   return $kalender;
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -58,6 +57,6 @@ function build_kalender($maand, $jaar, $datumArray){
 
 	</head>
 	<body>
-		<?php echo build_kalender(11, 2018); ?>
+		
 	</body>
 </html>
