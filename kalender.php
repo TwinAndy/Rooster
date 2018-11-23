27a857c75b5a5292c29fd468c384bf8e777f3179
Andrew Lodeizen <20040701@merletcollege.nl>
2018-11-23 11:12:34 +0100

    testtest

--- kalender.php
+++ kalender.php
@@ -4,7 +4,7 @@ class Kalender {
 	private $jaar;
 	private $dagen_vd_week;
 	private $aantal_dagen;
-	private $datum_info;
+	private $datum_maand;
 	private $dag_vd_week;
 /*
 	public function __construct($maand, $jaar, $dagen_vd_week = array('Maandag', 'Dinsdag', 'Woensdag', 'Donderdag', 'Vrijdag', 'Zaterdag', 'Zondag')){
@@ -19,14 +19,14 @@ class Kalender {
 		$this->jaar = $jaar;
 		$this->dagen_vd_week = $dagen_vd_week;
 		$this->aantal_dagen = cal_days_in_month( CAL_GREGORIAN, $this->maand, $this->jaar );
-		$this->datum_info = getdate(strtotime(mktime( 0,0,0,$this->maand,0,$this->jaar,0)));
+		$this->datum_maand = date("F", mktime(0,0,0, $this->maand, 0, 0))
 		$this->dag_vd_week = $this->datum_info['wday'];
 	}
 	public function show(){
 		// Maand en Jaar titel
 		$output = '<table class="kalender">';
 				//Maand naam word niet laten zien
-		$output .= '<caption>' . $this->datum_info[month] . ' ' . $this->jaar . '</caption>';
+		$output .= '<caption>' . $this->datum_maand/*[month]*/ . ' ' . $this->jaar . '</caption>';
 		$output .= '<tr>';
 
 		//Dagen van de week header
